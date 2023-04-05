d6a4834c7d51c84ccf1eaf768be3bc9f4ee6f967
Andrea Maugeri <andrewmaugeri@live.it>
2023-04-05 17:06:52 +0200

    modifiche varie

--- main.js
+++ main.js
@@ -23,3 +23,45 @@ window.addEventListener('scroll', ()=>{
         logoB.classList.add('d-none');
     }
 })
+
+
+// CHIAMATA ASINCRONA DI JAVASCRIPT
+
+function createInterval(finalNumber, element){
+    let counter = 0;
+    let interval = setInterval( ()=>{
+        if(counter < finalNumber){
+            counter=counter+11;
+            element.innerHTML = counter;
+        } else {
+            clearInterval(interval);
+        }
+    },1)
+}
+
+createInterval();
+
+// cattura degli span con numero 0
+
+let firstSpan = document.querySelector('#first-span');
+let secondSpan = document.querySelector('#second-span');
+let thirdSpan = document.querySelector('#third-span');
+
+let h2Test = document.querySelector('#h2Test');
+
+// variabile d'appoggio per cessare l'incremento dei numeri
+let intersectionCheck = true;
+// funzione intersection observe
+let observed = new IntersectionObserver(
+    (entries)=>{
+        entries.forEach( (entry)=>{
+            if(entry.isIntersecting && intersectionCheck == true){
+                createInterval(7659, firstSpan);
+                createInterval(9996, secondSpan);
+                createInterval(4567, thirdSpan);
+                intersectionCheck = false;
+            }
+        } )
+    }
+)  
+observed.observe(h2Test);
